5bddd974277008c446a1912524da15577e2cacd7
HagbardCeline <janpauldahlke@gmail.com>
2025-11-13 14:52:37 +0100

    before refactoring into click and solo routes

--- src/components/stock_list_item.rs
+++ src/components/stock_list_item.rs
@@ -1,19 +1,19 @@
 use dioxus::prelude::*;
-use crate::components::StockCard;
-use crate::models::StockQuote;
+use crate::models::get_stock_name;
 
 #[component]
 pub fn StockListItem(
     symbol: String,
-    quote: StockQuote,
     selected: bool,
     on_click: EventHandler<String>,
 ) -> Element {
+    let stock_name = get_stock_name(&symbol).unwrap_or_else(|| symbol.clone());
+    
     rsx! {
         div {
             class: if selected { "selected" } else { "" },
             onclick: move |_| on_click.call(symbol.clone()),
-            StockCard { symbol: symbol.clone(), quote }
+            div { class: "symbol", "{symbol} {stock_name}" }
         }
     }
 }
--- src/components/stocks_list.rs
+++ src/components/stocks_list.rs
@@ -1,20 +1,18 @@
 use dioxus::prelude::*;
 use crate::components::StockListItem;
-use crate::models::StockQuote;
 
 #[component]
 pub fn StocksList(
-    stocks: Vec<(String, StockQuote)>,
+    symbols: Vec<String>,
     selected_symbol: Signal<Option<String>>,
     on_symbol_select: EventHandler<String>,
 ) -> Element {
     rsx! {
         div { class: "stocks-container",
             div { class: "stocks-list",
-                for (symbol , quote) in stocks.iter() {
+                for symbol in symbols.iter() {
                     StockListItem {
                         symbol: symbol.clone(),
-                        quote: quote.clone(),
                         selected: selected_symbol() == Some(symbol.clone()),
                         on_click: move |sym| on_symbol_select.call(sym),
                     }
